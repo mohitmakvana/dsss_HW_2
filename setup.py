d5b7bc5b7ad6aefeabd847e6de99da622f6a949f
mohitmakvana <148951633+mohitmakvana@users.noreply.github.com>
2023-11-13 21:06:58 +0100

    Create setup.py
    setup file add

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup( name="Dsss_HW_2",
+      version="0.1", 
+      author="Mohit_Makvana", 
+      author_email="mohit.makvana@fau.de",
+      packages=find_packages(),
+      install_requires=["numpy", "turtles"] )
